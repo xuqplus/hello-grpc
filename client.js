ebceb21b5c9934e733151d9f954738427da141b9
Qunqun Xu <qunqun.xu@autodesk.com>
2020-11-10 16:13:44 +0800

    grpc demo client & server

---
+++ client.js
@@ -0,0 +1,34 @@
+var protoPath = `${__dirname}/demo.proto`;
+
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader')
+var packageDefinition = protoLoader.loadSync(protoPath, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+})
+
+var demoProto = grpc.loadPackageDefinition(packageDefinition)
+
+var main = function () {
+    var id, name;
+    if (process.argv.length > 2) {
+        id = process.argv[2];
+    } else {
+        id = 1;
+    }
+    if (process.argv.length > 3) {
+        name = process.argv[3];
+    } else {
+        name = 'xqq';
+    }
+
+    var client = new demoProto.user.User("localhost:50051", grpc.credentials.createInsecure())
+    client.f({ id, name }, (err, resp) => {
+        console.log(resp)
+    })
+}
+
+main()
